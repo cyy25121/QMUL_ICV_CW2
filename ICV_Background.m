ae764e442962aef3504d664778aefad20123f6fa
YY <y.chang@se16.qmul.ac.uk>
2016-11-24 00:27:23 +0000

    1. Changed to current/previous frame. 2. Added arrow head. 3. Added Background function.

---
+++ ICV_Background.m
@@ -0,0 +1,21 @@
+function [ bg, outImg , idx] = ICV_Background(v)
+    fr1 = readFrame(v);
+    fr2 = readFrame(v);
+    outImg = cat(4, fr1, fr2);
+    idx = 3;
+    while(hasFrame(v))
+        tmpfr = readFrame(v);
+        outImg(:,:,:,idx) = tmpfr;
+        idx = idx + 1;
+    end
+    video_size = size(outImg);
+    bg = uint8(zeros(video_size(1), video_size(2), video_size(3)));
+    for i=1:video_size(1)
+        for j=1:video_size(2)
+            for k=1:3
+                bg(i,j,k) = uint8(mode(outImg(i,j,k,:)));
+            end
+        end
+    end
+end
+
--- ICV_q4.m
+++ ICV_q4.m
@@ -3,7 +3,9 @@
 % Dataset: DatasetC
 disp('Intorduction of Computer Vision Coursework 2 - Q4: Motion estimation');
 clc; clear;
+%% Background
 v = VideoReader('Dataset/DatasetC.mpg');
+[bg, outImg, ~] = ICV_Background(v);
 
 %% Run
 
@@ -15,19 +17,20 @@ v = VideoReader('Dataset/DatasetC.mpg');
 %    fr2 = readFrame(v);
 %    figure; imshow(fr2); hold on
 %end
-
-drawArrow = @(x,y) quiver( x(1),y(1),x(2)-x(1),y(2)-y(1),0 , 'g');   
-
+v = VideoReader('Dataset/DatasetC.mpg');
+drawArrow = @(x,y) quiver( x(1),y(1),(x(2)-x(1)),(y(2)-y(1)), 3, 'r', 'MaxHeadSize', 100);   
+preFr = readFrame(v);
+%preFr = snapshot(cam);
+preFrg = preFr(:,:,1) * 0.299 + preFr(:,:,2) * 0.587 + preFr(:,:,3) * 0.114;
+k = 0;
 while(hasFrame(v))
-    fr1 = readFrame(v);    
-    fr1g = fr1(:,:,1) * 0.299 + fr1(:,:,2) * 0.587 + fr1(:,:,3) * 0.114;
-    if(hasFrame(v))
-        fr2 = readFrame(v);
-        fr2g = fr2(:,:,1) * 0.299 + fr2(:,:,2) * 0.587 + fr2(:,:,3) * 0.114;
-    end
-    imshow(fr2); hold on;
-    video_size = size(fr1g);
+    curFr = readFrame(v);
+    %curFr = snapshot(cam);
+    curFrg = curFr(:,:,1) * 0.299 + curFr(:,:,2) * 0.587 + curFr(:,:,3) * 0.114;
+    imshow(curFrg); hold on;
+    video_size = size(preFr);
     sum = 0;
+
 for i=1:16:video_size(1)
     for j=1:16:video_size(2)
         %fprintf('Block location: %d %d\n', i, j);
@@ -36,7 +39,7 @@ for i=1:16:video_size(1)
             for a=i-2:i+2
                 for b=j-2:j+2
                     if(a >= 1 && b >= 1 && a + 16 <= video_size(1) && b + 16 <= video_size(2))
-                        m = ICV_MSE(fr2g(i:i+16, j:j+16), fr1g(a:a+16, b:b+16));
+                        m = ICV_MSE(preFrg(i:i+16, j:j+16), curFrg(a:a+16, b:b+16));
                         if m < min
                             min = m;
                             min_m = [a, b];
@@ -50,11 +53,13 @@ for i=1:16:video_size(1)
             if(i ~= min_m(1) || j ~= min_m(2))
                 rectangle('Position', [j i 16 16], 'EdgeColor', 'b')
                 rectangle('Position', [min_m(2) min_m(1) 16 16], 'EdgeColor', 'r')
-                drawArrow([j min_m(2)+10*(j-min_m(2))/(i-min_m(1))]+8, [i min_m(1)+10]+8);
+                drawArrow([j min_m(2)]+8, [i min_m(1) ]+8);
             end
         end
     end
 end
     hold off;
+    preFr = curFr;
+    preFrg = curFrg;
 end
 
